88c34c2e0090e94b08a0b54b5ed1906dae30da93
Brian Atwood <batwood011@gmail.com>
2016-03-05 00:28:30 -0600

    make arguments work

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,9 @@ var babelify = require('babel-core/register');
 gulp.task('hist', function() {
   babelify();
   var hist = require('./devtools/tasks').hist;
-  var array = argv.array || [1,1,2,3,3,3,3,4,5,5];
+  var array = JSON.parse(argv.array) || [1,1,2,3,3,3,3,4,5,5];
   var opts = {
-    binCount: argv.binCount || 5
+    binCount: parseInt(argv.binCount) || 5
   }
   hist(array, opts);
 });
